1543fdb7bdb74d3f24cf534e4cf4cf589ff30f71
Ashish <45767581+vattevaii@users.noreply.github.com>
2022-09-16 14:09:00 +0545

    asd

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,10 +4,20 @@ import { NavLink } from "react-router-dom";
 function NavBar() {
   return (
     <>
-      <Box>
-        <Flex>
+      <Box
+        width={"100vw"}
+        py="1em"
+        background={"linear-gradient(90deg,red,yellow)"}
+      >
+        <Flex justifyContent={"space-evenly"}>
           <Link as={NavLink} to="transactions">
             Transactions
+          </Link>
+          <Link as={NavLink} to="transactions">
+            Blocks
+          </Link>
+          <Link as={NavLink} to="transactions">
+            Blockchain
           </Link>
         </Flex>
       </Box>
