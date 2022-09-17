4f9ba683cb36f5a84d29db3552b503e4e774d4e8
Ashish <45767581+vattevaii@users.noreply.github.com>
2022-09-17 12:42:23 +0545

    add transaction

--- src/AppRouters/index.tsx
+++ src/AppRouters/index.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BaseLayout from "../components/Layouts/BaseLayout";
 import Blocks from "../routes/Blocks";
+import NoPage from "../routes/NoPagge";
 import Transactions from "../routes/Transactions";
 
 function Approuters() {
@@ -9,7 +10,9 @@ function Approuters() {
       <Routes>
         <Route path="/" element={<BaseLayout />}>
           <Route path="transactions" element={<Transactions />}></Route>
-          <Route path="blocks" element={<Blocks />}></Route>
+          <Route path="blocks/:id" element={<Blocks />} />
+          <Route path="blocks" element={<Blocks />} />
+          <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
